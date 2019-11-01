69559a58e521d4498836f3764ec6e5e3dd6f4ce9
mismo123 <z5116810@student.unsw.edu.au>
2019-11-02 01:01:03 +1100

    boulder tests done

--- src/unsw/tests/BoulderTest.java
+++ src/unsw/tests/BoulderTest.java
@@ -4,11 +4,56 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import unsw.dungeon.Boulder;
+import unsw.dungeon.Dungeon;
+import unsw.dungeon.Player;
+import unsw.dungeon.Wall;
+import unsw.dungeon.playerObserver;
+
 class BoulderTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testPush() {
+		Player player = new Player(new Dungeon(4,4), 0, 0);
+		Boulder boulder = new Boulder(1,0);
+		player.addObserver((playerObserver)boulder);
+		player.moveRight();
+		assertEquals(boulder.getX(), 2);
+		assertEquals(boulder.getY(), 0);
+		assertEquals(player.getX(), 1);
+		assertEquals(player.getY(), 0);
 	}
 
+	@Test
+	void testTwoBoulders() {
+		Player player = new Player(new Dungeon(4,4), 0, 0);
+		Boulder boulder = new Boulder(1,0);
+		Boulder boulder2 = new Boulder(2,0);
+		player.addObserver((playerObserver)boulder);
+		player.addObserver((playerObserver)boulder2);
+		boulder.addObserver((playerObserver)boulder2);
+		boulder2.addObserver((playerObserver)boulder);
+		player.moveRight();
+		assertEquals(boulder.getX(), 1);
+		assertEquals(boulder.getY(), 0);
+		assertEquals(boulder2.getX(), 2);
+		assertEquals(boulder2.getY(), 0);
+		assertEquals(player.getX(), 0);
+		assertEquals(player.getY(), 0);
+	}
+	
+	@Test
+	void testBoulderNextToWall() {
+		Player player = new Player(new Dungeon(4,4), 0, 0);
+		Boulder boulder = new Boulder(1,0);
+		Wall wall = new Wall(2,0);
+		player.addObserver((playerObserver)boulder);
+		player.addObserver((playerObserver)wall);
+		boulder.addObserver((playerObserver)wall);
+		player.moveRight();
+		assertEquals(boulder.getX(), 1);
+		assertEquals(boulder.getY(), 0);
+		assertEquals(player.getX(), 0);
+		assertEquals(player.getY(), 0);
+	}	
 }
