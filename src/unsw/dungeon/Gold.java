938d201cd90a895f37e114dc840ef321528d82be
Daniel Huang <danielhuang4739@gmail.com>
2019-11-03 01:57:14 +1100

    added comments for gold and exit

--- src/unsw/dungeon/Gold.java
+++ src/unsw/dungeon/Gold.java
@@ -3,10 +3,18 @@ package unsw.dungeon;
 public class Gold extends Entity implements playerObserver{
 	boolean deleted = false;
 	
+	/**
+	 * Constructor for the gold entity
+	 * @param x X position of the gold in the dungeon
+	 * @param y Y position of the gold in the dungeon
+	 */
     public Gold(int x, int y) {
         super(x, y);
     }
 
+    /**
+     * If the player walks over the gold, the player will collect the gold
+     */
     @Override
     public void update(playerSubject obj, int dX, int dY) {
     	if (obj instanceof Player) {
