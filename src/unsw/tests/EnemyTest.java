074f0f2997534b8cc27ba4bb59946055cbf43cfd
Daniel Huang <danielhuang4739@gmail.com>
2019-11-02 14:15:40 +1100

    made tests for potion and sword

--- src/unsw/tests/EnemyTest.java
+++ src/unsw/tests/EnemyTest.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import unsw.dungeon.Dungeon;
+import unsw.dungeon.Enemy;
 import unsw.dungeon.Player;
+import unsw.dungeon.Potion;
 import unsw.dungeon.Sword;
 
 class EnemyTest {
@@ -13,28 +15,76 @@ class EnemyTest {
 	@Test
 	void deathBattleTest() {
 		Dungeon dungeon = new Dungeon(100, 100);
-		Player player = new Player(dungeon,5,5);
-		Sword sword = new Sword(4,4);
-		player.setSword(sword);
-		assertEquals(sword,player.getSword() , "add test 2");
+		Player player = new Player(dungeon,5,4);
+		Enemy enemy = new Enemy(5,6);
+		player.addObserver(enemy);
+		player.moveDown();
+		player.moveDown();
+		assertEquals(false,player.isAlive(), "add test 2");
+	}
+	
+	@Test
+	void healthTest() {
+		Dungeon dungeon = new Dungeon(100, 100);
+		Player player = new Player(dungeon,5,4);
+		Enemy enemy = new Enemy(5,6);
+		player.addObserver(enemy);
+		player.moveDown();
+		player.moveDown();
+		assertEquals(false,enemy.isDeleted(), "add test 2");
 	}
 	
 	@Test
 	void potionBattleTest() {
 		Dungeon dungeon = new Dungeon(100, 100);
 		Player player = new Player(dungeon,5,5);
-		Sword sword = new Sword(4,4);
-		player.setSword(sword);
-		assertEquals(sword,player.getSword() , "add test 2");
+		Potion potion = new Potion(5,6);
+		Enemy enemy = new Enemy(5,7);
+		player.addObserver(enemy);
+		player.addObserver(potion);
+		player.moveDown();
+		player.moveDown();
+		assertEquals(true,player.isAlive() , "add test 2");
+	}
+	
+	@Test
+	void healthTest2() {
+		Dungeon dungeon = new Dungeon(100, 100);
+		Player player = new Player(dungeon,5,5);
+		Potion potion = new Potion(5,6);
+		Enemy enemy = new Enemy(5,7);
+		player.addObserver(enemy);
+		player.addObserver(potion);
+		player.moveDown();
+		player.moveDown();
+		assertEquals(true,enemy.isDeleted(), "add test 2");
 	}
 	
 	@Test
 	void swordBattleTest() {
 		Dungeon dungeon = new Dungeon(100, 100);
 		Player player = new Player(dungeon,5,5);
-		Sword sword = new Sword(4,4);
-		player.setSword(sword);
-		assertEquals(sword,player.getSword() , "add test 2");
+		Sword sword = new Sword(5,6);
+		Enemy enemy = new Enemy(5,7);
+		player.addObserver(enemy);
+		player.addObserver(sword);
+		player.moveDown();
+		player.moveDown();
+		assertEquals(true,player.isAlive() , "add test 2");
 	}
+	
+	@Test
+	void healthTest3() {
+		Dungeon dungeon = new Dungeon(100, 100);
+		Player player = new Player(dungeon,5,5);
+		Sword sword = new Sword(5,6);
+		Enemy enemy = new Enemy(5,7);
+		player.addObserver(enemy);
+		player.addObserver(sword);
+		player.moveDown();
+		player.moveDown();
+		assertEquals(true,enemy.isDeleted() , "add test 2");
+	}
+
 
 }
