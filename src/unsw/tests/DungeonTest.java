8a2e5a0dac2672f91054d691cf9fcb398c5c1636
Daniel Huang <danielhuang4739@gmail.com>
2019-11-02 22:01:19 +1100

    added test files

---
+++ src/unsw/tests/DungeonTest.java
@@ -0,0 +1,14 @@
+package unsw.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DungeonTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
