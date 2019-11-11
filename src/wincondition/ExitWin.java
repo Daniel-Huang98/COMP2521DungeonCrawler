9518355cb1724701ae49b6ceb4007ab4aa3a8868
Daniel Huang <Daniel.Huang4739@gmail.com>
2019-11-11 17:59:37 +1100

    no longer crashes when check goals

---
+++ src/wincondition/ExitWin.java
@@ -0,0 +1,13 @@
+package wincondition;
+
+import unsw.dungeon.Dungeon;
+
+public class ExitWin implements WinCondition{
+
+	@Override
+	public boolean canWin(Dungeon dungeon) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
